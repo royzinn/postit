9edaf156358b21f514787c3234975ad32f12a9e0
Roy Zinn <roy.zinn@gmail.com>
2013-04-13 21:17:54 +0300

    adding resources and creating posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,19 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def new
+  	Post.new
+  end
+
+  def create
+  	@post = Post.new(params[:post])
+  	@post.user = User.first
+
+  	if @post.save
+  		flash[:notice] = "Successfully created new post"
+  		redirect_to root_url
+  	else
+  		render :new
+  	end
 end
